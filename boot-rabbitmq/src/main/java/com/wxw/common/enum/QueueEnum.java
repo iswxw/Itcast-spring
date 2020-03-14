886af4e2de66d88a48ec3ee89f6cdbd8c10600e2
水墨青花 <1550518923@qq.com>
2020-03-15 01:43:13 +0800

    Springboot整合swagger2

---
+++ boot-rabbitmq/src/main/java/com/wxw/common/enum/QueueEnum.java
@@ -0,0 +1,37 @@
+package com.wxw.common;
+
+import lombok.Getter;
+
+/**
+ * 消息队列枚举配置
+ */
+@Getter
+public enum QueueEnum {
+    /**
+     * 消息通知队列
+     */
+    QUEUE_ORDER_CANCEL("bs.order.direct", "bs.order.cancel", "bs.order.cancel"),
+    /**
+     * 消息通知ttl队列
+     */
+    QUEUE_TTL_ORDER_CANCEL("bs.order.direct.ttl", "bs.order.cancel.ttl", "bs.order.cancel.ttl");
+
+    /**
+     * 交换名称
+     */
+    private String exchange;
+    /**
+     * 队列名称
+     */
+    private String name;
+    /**
+     * 路由键
+     */
+    private String routeKey;
+
+    QueueEnum(String exchange, String name, String routeKey) {
+        this.exchange = exchange;
+        this.name = name;
+        this.routeKey = routeKey;
+    }
+}
