4f6dd78d7a3c3d7fe85212abe21e6be40018763b
水墨青花 <1550518923@qq.com>
2020-04-11 15:35:47 +0800

    实现聚合和子聚合功能

---
+++ itcast-elasticsearch/src/test/java/com/wxw/AggregatedTest.java
@@ -0,0 +1,91 @@
+package com.wxw;
+
+import com.wxw.domain.Item;
+import com.wxw.service.ItemResponstory;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.metrics.avg.InternalAvg;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+
+import java.util.List;
+
+/**
+ * @Author: wxw
+ * @create: 2020-04-11-14:56
+ * 聚合为桶
+ */
+@SpringBootTest
+public class AggregatedTest {
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Autowired
+    private ItemResponstory itemResponstory;
+
+    /**
+     * 聚合为桶
+     */
+    @Test
+    public void TestAggregatedOne(){
+        //初始化自定义查询构建器
+        NativeSearchQueryBuilder  queryBuilder = new NativeSearchQueryBuilder();
+        //添加聚合
+        queryBuilder.addAggregation(AggregationBuilders.terms("brandAgg").field("brand"));
+        //添加结果集过滤，不包括任何字段
+        queryBuilder.withSourceFilter(new FetchSourceFilter(new String[]{},null));
+        //执行聚合查询
+        AggregatedPage<Item> items = (AggregatedPage<Item>)this.itemResponstory.search(queryBuilder.build());
+        //解析聚合结果集,根据聚合的类型以及字段类型进行强转    聚合类型——词条聚合 brandAgg—通过聚合名称获取聚合对象
+        StringTerms brandAgg = (StringTerms)items.getAggregation("brandAgg");
+        //获得桶的聚合
+        List<StringTerms.Bucket> buckets = brandAgg.getBuckets();
+        buckets.forEach(bucket -> {
+            System.out.println(bucket.getKeyAsString());
+            System.out.println(bucket.getDocCount());
+        });
+    }
+
+    /**
+     * 聚合为桶内度量 （子聚合）
+     */
+    @Test
+    public void testSubAgg(){
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 不查询任何结果
+        queryBuilder.withSourceFilter(new FetchSourceFilter(new String[]{""}, null));
+
+        // 1、添加一个新的聚合，聚合类型为terms，聚合名称为brands，聚合字段为brand
+        queryBuilder.addAggregation(
+                AggregationBuilders.terms("brands").field("brand")
+                        .subAggregation(AggregationBuilders.avg("priceAvg").field("price")) // 在品牌聚合桶内进行嵌套聚合，求平均值
+        );
+        // 2、查询,需要把结果强转为AggregatedPage类型
+        AggregatedPage<Item> aggPage = (AggregatedPage<Item>) this.itemResponstory.search(queryBuilder.build());
+        // 3、解析
+        // 3.1、从结果中取出名为brands的那个聚合，
+        // 因为是利用String类型字段来进行的term聚合，所以结果要强转为StringTerm类型
+        StringTerms agg = (StringTerms) aggPage.getAggregation("brands");
+        // 3.2、获取桶
+        List<StringTerms.Bucket> buckets = agg.getBuckets();
+        // 3.3、遍历
+        for (StringTerms.Bucket bucket : buckets) {
+            // 3.4、获取桶中的key，即品牌名称  3.5、获取桶中的文档数量
+            System.out.println(bucket.getKeyAsString() + "，共" + bucket.getDocCount() + "台");
+
+            // 3.6.获取子聚合结果：
+            InternalAvg avg = (InternalAvg) bucket.getAggregations().asMap().get("priceAvg");
+            System.out.println("平均售价：" + avg.getValue());
+        }
+
+    }
+
+}
